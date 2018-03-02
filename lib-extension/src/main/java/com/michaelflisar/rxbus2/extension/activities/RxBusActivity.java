8bc35dc8529d6ddb7c641f33af2a5df5e16a54b0
MFlisar <mflisar.development@gmail.com>
2018-03-02 20:32:46 +0100

    extension added

---
+++ lib-extension/src/main/java/com/michaelflisar/rxbus2/extension/activities/RxBusActivity.java
@@ -0,0 +1,48 @@
+package com.michaelflisar.rxbus2.extension.activities;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.michaelflisar.rxbus2.interfaces.IRxBusQueue;
+import com.michaelflisar.rxbus2.rx.RxDisposableManager;
+
+import org.reactivestreams.Publisher;
+
+import io.reactivex.processors.BehaviorProcessor;
+
+public class RxBusActivity extends AppCompatActivity implements IRxBusQueue {
+
+    private final BehaviorProcessor<Boolean> resumeStateProcessor = BehaviorProcessor.createDefault(false);
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        resumeStateProcessor.onNext(true);
+    }
+
+    @Override
+    public void onPause() {
+        resumeStateProcessor.onNext(false);
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        RxDisposableManager.unsubscribe(this);
+        super.onDestroy();
+    }
+
+    // --------------
+    // RXBus
+    // --------------
+
+    @Override
+    public boolean isBusResumed() {
+        return resumeStateProcessor.getValue();
+    }
+
+    @Override
+    public Publisher<Boolean> getResumeObservable() {
+        return resumeStateProcessor;
+    }
+}
